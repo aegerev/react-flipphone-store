ec9fd9d91bdcc895d2a50760a30187236c0874cc
alex <alexandersegerev@gmail.com>
2024-08-08 11:19:51 -0400

    Added a new component; updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { ItemList } from './components/ItemList.js';
+import { Cart } from './Cart';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+	const [cartItems, setCartItems] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const addItemToCart = (item) => {
+		const exist = cartItems.find((x) => x.id === item.id);
+		if (exist) {
+			setCartItems(
+				cartItems.map((x) =>
+					x.id === item.id ? { ...exist, qty: exist.qty + 1 } : x
+				)
+			);
+		} else {
+			setCartItems([...cartItems, { ...item, qty: 1 }]);
+		}
+	};
+
+	return (
+		<div className="app">
+			<h1>Shopping Cart</h1>
+			<div className="content">
+				<ItemList addItemToCart={addItemToCart} />
+				<Cart cartItems={cartItems} />
+			</div>
+		</div>
+	);
 }
 
 export default App
